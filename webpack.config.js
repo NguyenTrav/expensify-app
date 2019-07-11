e27a3122fb2986a0aa76ac9dd64388dcf472db0b
Travis <NguyenTrav@gmail.com>
2019-07-10 21:49:54 -0400

    setup prod webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = (env) =>
 {
     const isProduction = env === "production";
+    const CSSExtract = new ExtractTextPlugin("styles.css");
 
     return {
 
@@ -23,14 +25,30 @@ module.exports = (env) =>
             {
                 //target folders end in css
                 test: /\.s?css$/,
-                use: [
-                    "style-loader",
-                    "css-loader", 
-                    "sass-loader"
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+
             }]
         },
-            devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+
+        plugins: [
+            CSSExtract
+        ],
+            devtool: isProduction ? "source-map" : "inline-source-map",
             devServer: 
             {
                 contentBase: path.join(__dirname, 'public'), 

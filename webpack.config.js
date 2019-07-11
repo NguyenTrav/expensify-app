ab237a8520e91496bd3a44b6f119f739c8e6755e
Travis <NguyenTrav@gmail.com>
2019-07-10 21:26:45 -0400

    prod webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,42 @@
 const path = require('path');
 
-module.exports = 
+module.exports = (env) =>
 {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-},
+    const isProduction = env === "production";
 
-module: 
-{
-    rules: [{
-        loader: "babel-loader",
-        // creates rule to check everytime .js file, run babel
-        test: /\.js$/,
-        exclude: /node_modules/
-    },
-    {
-        //target folders end in css
-        test: /\.s?css$/,
-        use: [
-            "style-loader",
-            "css-loader", 
-            "sass-loader"
-        ]
-    }]
-},
-    devtool: "cheap-module-eval-source-map",
-    devServer: 
-    {
-        contentBase: path.join(__dirname, 'public'), 
-        historyApiFallback: true
-    }
+    return {
+
+          entry: './src/app.js',
+          output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        
+        module: 
+        {
+            rules: [{
+                loader: "babel-loader",
+                // creates rule to check everytime .js file, run babel
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                //target folders end in css
+                test: /\.s?css$/,
+                use: [
+                    "style-loader",
+                    "css-loader", 
+                    "sass-loader"
+                ]
+            }]
+        },
+            devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+            devServer: 
+            {
+                contentBase: path.join(__dirname, 'public'), 
+                historyApiFallback: true
+            }
+        };
 };
+
+

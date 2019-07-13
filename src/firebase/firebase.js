0e42fec62810e3f6c7f38f54de12e28b7dd0bb11
Travis <NguyenTrav@gmail.com>
2019-07-13 18:12:02 -0400

    wow tests are bad

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    apiKey: "AIzaSyBbcVNwnfPDMWpz2mIoN_2l0ql8l8VRShw",
+    authDomain: "expensify-b4659.firebaseapp.com",
+    databaseURL: "https://expensify-b4659.firebaseio.com",
+    projectId: "expensify-b4659",
+    storageBucket: "expensify-b4659.appspot.com",
+    messagingSenderId: "267938021419",
     appId: "1:267938021419:web:cf769f7a3010e010"
   };
 
@@ -17,8 +17,6 @@ const firebaseConfig = {
     firebase.initializeApp(firebaseConfig);
     
     const database = firebase.database();
-
-    export { firebase, database as default};
 
     // database.ref().set({
     //     name: "Travis Nguyen",
@@ -123,11 +121,11 @@ const firebaseConfig = {
 //     body: "lets party"
 // });
 
-// database.ref("expenses").push({
-//     description: "water bill",
-//     amount: 10,
-//     date: 1021994,
-// });
+database.ref("expenses").push({
+    description: "water bill",
+    amount: 10,
+    date: 1021994,
+});
 
 // database.ref("expenses").on("value", (snapshot) => 
 // {

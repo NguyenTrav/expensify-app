28562fdae7de1c0c640f34aa6d8a865b75ad88f8
Travis <NguyenTrav@gmail.com>
2019-07-16 13:50:56 -0400

    finished css and loader

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -56,21 +56,25 @@ export default class ExpenseForm extends React.Component {
   };
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+
+        <form className="form" onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form_error">{this.state.error}</p>}
+
           <input
             type="text"
             placeholder="Description"
             autoFocus
             value={this.state.description}
             onChange={this.onDescriptionChange}
+            className="text-input"
           />
           <input
             type="text"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
+            className="text-input"
+
           />
           <SingleDatePicker
             date={this.state.createdAt}
@@ -84,11 +88,16 @@ export default class ExpenseForm extends React.Component {
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
+            className="textarea"
+            
+
           >
           </textarea>
-          <button>Add Expense</button>
+          <div>
+            <button className="button">Save Expense</button>
+          </div>
+          
         </form>
-      </div>
     )
   }
 }
